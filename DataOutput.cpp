86d1ec5d7081445fffc125db39b670e8bfef3129
harrisonstark <66219289+harrisonstark@users.noreply.github.com>
2022-04-18 17:37:45 -0400

    Update DataOutput.cpp

--- DataOutput.cpp
+++ DataOutput.cpp
@@ -10,15 +10,13 @@ int main()
     time_t itr = time(NULL);
     fstream file;
     file.open("data.csv");
+    fstream fileOut("dataOut.csv", std::ios_base::app | std::ios_base::in);
     string line = "";
-    vector<string> lines;
-    vector<vector<string>> data;
     getline(file, line);
-    if (file.is_open()) {
+    if (file.is_open() && fileOut.is_open()) {
         while (getline(file, line)) {
-            lines.push_back(line);
             vector<double> locs;
-            vector<string> dataToInsert;
+            string s = "";
             string sev;
             string temp = line.substr(0, 200);
             for (int j = 0; j < 7; j++) {
@@ -28,27 +26,12 @@ int main()
                 else if (j > 2)
                     locs.push_back(stod(temp.substr(0, temp.find(','))));
             }
-            dataToInsert.push_back(to_string((locs.at(0) + locs.at(2)) / 2));
-            dataToInsert.push_back(to_string((locs.at(1) + locs.at(3)) / 2));
-            dataToInsert.push_back(sev);
-            data.push_back(dataToInsert);
+            s += to_string((locs.at(0) + locs.at(2)) / 2) + "," + to_string((locs.at(1) + locs.at(3)) / 2) + "," + sev;
+            fileOut << s << "\n";
         }
         file.close();
-    }
-    time_t ftr = time(NULL);
-    cout << "Time taken to load file: " << ftr - itr << " seconds." << endl;
-    time_t itw = time(NULL);
-    fstream fileOut("dataOut.csv", std::ios_base::app | std::ios_base::in);
-    if (fileOut.is_open()) {
-        for (auto x : data) {
-            string s = "";
-            for (auto y : x) {
-                s += y + ",";
-            }
-            fileOut << s.substr(0, s.length() - 1) << "\n";
-        }
         fileOut.close();
     }
-    time_t ftw = time(NULL);
-    cout << "Time taken to output file: " << ftw - itw << " seconds." << endl;
+    time_t ftr = time(NULL);
+    cout << "Time taken to load, read, and write file: " << ftr - itr << " seconds." << endl;
 }
