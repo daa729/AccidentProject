c168307b34be3cd8eb09fe4ab22463487e41451f
tankz box <colbyjack@tankz.xyz.com>
2022-04-19 15:46:58 -0700

    Some more small changes

--- getData.php
+++ getData.php
@@ -1,10 +1,17 @@
 <?php
 $lat = $_GET['lat'];
 $long = $_GET['long'];
-if(!is_numeric($lat) || !is_numeric($long)){
+$amount = $_GET['amount'];
+if(!is_numeric($lat) || !is_numeric($long) || !is_numeric($amount)){
 	//no command injection plz
 	return;
 }
-$output = shell_exec("./AccidentGrabber '".$lat."' '".$long."' 1000");
+if($amount < 1){
+	$amount = 1;
+}
+else if($amount > 2845342){
+	$amount = 2845342;
+}
+$output = shell_exec("./AccidentGrabber '".$lat."' '".$long."' '".$amount."'");
 echo $output;
 ?>
